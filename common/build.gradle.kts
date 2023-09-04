5136de86a42420023bd562bfcc465ff86e3e0c8f
Ivan Karlo Rukavina <ivankarlo.rukavina@gmail.com>
2023-09-05 00:04:05 +0200

    update build.gradle.kts for modules to version catalog

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -35,27 +35,29 @@ android {
 }
 
 dependencies {
-    //implementation(project(":app"))
+    implementation(project(":common"))
 
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    //noinspection GradleDependency
-    implementation("com.google.android.material:material:1.8.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-    //noinspection GradleDependency
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
-    //noinspection GradleDependency
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
-    implementation("com.google.dagger:hilt-android:2.44")
-    kapt("com.google.dagger:hilt-android-compiler:2.44")
-    implementation ("androidx.compose.ui:ui:1.5.0")
-    implementation ("androidx.compose.material:material:1.5.0")
-    implementation ("androidx.compose.runtime:runtime-livedata:1.5.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(libs.androidxCoreKtx)
+    implementation(libs.lifecycleKtx)
+    implementation(libs.activity.compose)
+    implementation(platform(libs.androidxComposeBom))
+    implementation(libs.composeUi)
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation(libs.daggerHilt)
+    kapt(libs.daggerHiltCompiler)
+    implementation(libs.composeUi)
+    implementation(libs.composeMaterial)
+    implementation(libs.composeLiveData)
+    implementation("androidx.compose.material3:material3")
+    implementation(libs.fragment.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.testExtJunit)
+    androidTestImplementation(libs.espressoCore)
+    androidTestImplementation(platform(libs.androidxComposeBom))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
 
 kapt {
