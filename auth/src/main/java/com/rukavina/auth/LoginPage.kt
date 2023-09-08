bd9465db4049d4fd33d2e4b305069b137bff3c01
Ivan Karlo Rukavina <ivankarlo.rukavina@gmail.com>
2023-09-09 01:39:35 +0200

    add login screen

---
+++ auth/src/main/java/com/rukavina/auth/LoginPage.kt
@@ -0,0 +1,114 @@
+package com.rukavina.auth
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.*
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
+@Composable
+fun LoginPage(
+    onLoginClick: () -> Unit,
+    onSignUpClick: () -> Unit
+) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    val emailFocusRequester = remember { FocusRequester() }
+    val passwordFocusRequester = remember { FocusRequester() }
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(R.string.welcome_auth),
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+                .focusRequester(emailFocusRequester),
+            singleLine = true,
+            placeholder = { Text(text = "Email/Username") },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(
+                onNext = { emailFocusRequester.requestFocus() }
+            )
+        )
+
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+                .focusRequester(passwordFocusRequester),
+            singleLine = true,
+            placeholder = { Text(text = "Password") },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Done,
+                keyboardType = KeyboardType.Password
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    passwordFocusRequester.requestFocus()
+                    keyboardController?.hide()
+                    onLoginClick()
+                }
+            )
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = { onLoginClick() },
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Text(text = "Log In")
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        TextButton(
+            onClick = { onSignUpClick() },
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Text(text = "Don't have an account? Sign up")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LoginPagePreview() {
+    LoginPage(
+        onLoginClick = {},
+        onSignUpClick = {}
+    )
+}
