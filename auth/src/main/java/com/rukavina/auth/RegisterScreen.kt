dc6ba7bf0ed61c9876d25ba3f9b6a9eeefe5aa02
Ivan Karlo Rukavina <ivankarlo.rukavina@gmail.com>
2023-09-10 00:44:55 +0200

    add register screen

---
+++ auth/src/main/java/com/rukavina/auth/RegisterScreen.kt
@@ -0,0 +1,139 @@
+package com.rukavina.auth
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.*
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
+@Composable
+fun RegistrationPage(
+    onSignUpClick: () -> Unit,
+    onLoginClick: () -> Unit
+) {
+    var email by remember { mutableStateOf("") }
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
+    var isPasswordVisible by remember { mutableStateOf(false) }
+    var isConfirmPasswordVisible by remember { mutableStateOf(false) }
+
+    val emailFocusRequester = remember { FocusRequester() }
+    val usernameFocusRequester = remember { FocusRequester() }
+    val passwordFocusRequester = remember { FocusRequester() }
+    val confirmPasswordFocusRequester = remember { FocusRequester() }
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(R.string.welcome_auth),
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+                .focusRequester(emailFocusRequester),
+            singleLine = true,
+            placeholder = { Text(text = "Email") },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(
+                onNext = { usernameFocusRequester.requestFocus() }
+            )
+        )
+
+        TextField(
+            value = username,
+            onValueChange = { username = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+                .focusRequester(usernameFocusRequester),
+            singleLine = true,
+            placeholder = { Text(text = "Username") },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(
+                onNext = { passwordFocusRequester.requestFocus() }
+            )
+        )
+
+        PasswordTextField(
+            value = password,
+            onValueChange = { password = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+                .focusRequester(passwordFocusRequester),
+            placeholder = { Text(text = "Password") },
+            onPasswordToggleClick = { isPasswordVisible = !isPasswordVisible },
+            isPasswordVisible = isPasswordVisible
+        )
+
+        PasswordTextField(
+            value = confirmPassword,
+            onValueChange = { confirmPassword = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+                .focusRequester(confirmPasswordFocusRequester),
+            placeholder = { Text(text = "Confirm Password") },
+            onPasswordToggleClick = { isConfirmPasswordVisible = !isConfirmPasswordVisible },
+            isPasswordVisible = isConfirmPasswordVisible
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = { /* Handle registration here */ },
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Text(text = "Sign Up")
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        TextButton(
+            onClick = { onLoginClick() },
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Text(text = "Already have an account? Log in")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun RegistrationPagePreview() {
+    RegistrationPage(
+        onSignUpClick = {},
+        onLoginClick = {}
+    )
+}
+
