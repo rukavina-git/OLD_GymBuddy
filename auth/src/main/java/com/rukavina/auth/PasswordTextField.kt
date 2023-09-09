6311b8954c96b2c67f9939778114874a8733db26
Ivan Karlo Rukavina <ivankarlo.rukavina@gmail.com>
2023-09-10 00:44:18 +0200

    add password text-field

---
+++ auth/src/main/java/com/rukavina/auth/PasswordTextField.kt
@@ -0,0 +1,58 @@
+package com.rukavina.auth
+
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.runtime.*
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.material.icons.*
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.ui.text.*
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun PasswordTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    placeholder: @Composable () -> Unit = {},
+    isPasswordVisible: Boolean,
+    onPasswordToggleClick: () -> Unit
+) {
+    var isPasswordVisible by remember { mutableStateOf(isPasswordVisible) }
+
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        modifier = modifier,
+        singleLine = true,
+        trailingIcon = {
+            IconButton(
+                onClick = { onPasswordToggleClick() },
+            ) {
+                Icon(
+                    imageVector = if (isPasswordVisible) Icons.Default.VisibilityOff else Icons.Default.Visibility,
+                    contentDescription = if (isPasswordVisible) {
+                        stringResource(id = R.string.password_visible)
+                    } else {
+                        stringResource(id = R.string.password_hidden)
+                    }
+                )
+            }
+        },
+        placeholder = placeholder,
+        keyboardOptions = KeyboardOptions(
+            imeAction = androidx.compose.ui.text.input.ImeAction.Done,
+            keyboardType = KeyboardType.Password
+        ),
+        visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+    )
+}
