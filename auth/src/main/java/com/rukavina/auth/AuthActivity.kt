af80c8bff3dc63a54df8459aa3cff06497e781cc
Ivan Karlo Rukavina <ivankarlo.rukavina@gmail.com>
2023-09-10 03:33:38 +0200

    add navigation for auth

--- auth/src/main/java/com/rukavina/auth/AuthActivity.kt
+++ auth/src/main/java/com/rukavina/auth/AuthActivity.kt
@@ -3,13 +3,9 @@ package com.rukavina.auth
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.rukavina.auth.ui.theme.GymBuddyTheme
 
 class AuthActivity : ComponentActivity() {
@@ -17,27 +13,18 @@ class AuthActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GymBuddyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Auth screen.")
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "login") {
+                    composable("login") { LoginScreen(
+                        onLoginClick = {},
+                        navController
+                    ) }
+                    composable("register") { RegistrationScreen(
+                        onSignUpClick = {},
+                        navController
+                    ) }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GymBuddyTheme {
-        Greeting("Android")
     }
 }
