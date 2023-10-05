2397e8d42f3b65dc60001ac8c3085f39793829ff
Ivan Karlo Rukavina <ivankarlo.rukavina@gmail.com>
2023-10-06 00:17:25 +0200

    refactor MainActivity

--- app/src/main/java/com/rukavina/gymbuddy/MainActivity.kt
+++ app/src/main/java/com/rukavina/gymbuddy/MainActivity.kt
@@ -8,30 +8,23 @@ import com.google.firebase.FirebaseApp
 import com.rukavina.auth.AuthActivity
 import com.rukavina.exercise.model.entities.Exercise
 import com.rukavina.exercise.ui.ExerciseViewModel
-import com.rukavina.gymbuddy.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
     private val exerciseViewModel: ExerciseViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Create an Intent to launch AuthActivity
-        val intent = Intent(this, AuthActivity::class.java)
         // Start AuthActivity
+        val intent = Intent(this, AuthActivity::class.java)
         startActivity(intent)
 
         // Initialize Firebase
         FirebaseApp.initializeApp(this);
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
 
         exerciseViewModel.addExercise(Exercise(1, "squat", "legs", "none"))
         exerciseViewModel.addExercise(Exercise(2, "deadlift", "back", "good for back and legs"))
